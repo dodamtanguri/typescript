76fb51515032daf3fde776c8c92de4884ae5f044
sohee <sh663746@kpcard.co.kr>
2024-07-12 17:01:03 +0900

    19.튜플작업하기

--- app.ts
+++ app.ts
@@ -2,11 +2,23 @@
 //     name : string,
 //     age : number
 // } = {
- const person ={
+ const person :{
+    name : string,
+    age: number,
+    hobbies : string[],
+    //Tuple : 타입스크립트에 정확히 두 개의 요소가 있는 특별한 배열
+    role : [number,string] 
+ } ={
     name : "sohee",
     age :30,
-    hobbies : ['Sports', 'Cooking']
+    hobbies : ['Sports', 'Cooking'],
+    role :[2, 'author'],
 };
+//Tuple에서 허용되는 일종의 예외, 타입스크립트가 이 오류를 잡지 못함. 
+//person.role.push('admin');
+//person.role[1] = 10;
+
+//person.role = [0, 'admin', 'user'];
 
 let favoriteActivities: string[];
 
