2fbeb0f14afff4e3dcdf8081c567fbce81d3a6cd
sohee <sh663746@kpcard.co.kr>
2024-07-15 16:53:38 +0900

    53.나머지매개변수

--- src/section4_javascript_typescript/app.ts
+++ src/section4_javascript_typescript/app.ts
@@ -1,16 +1,16 @@
-const addArrow = (a:number, b:number = 1) => a+b;
+// const addArrow = (a:number, b:number = 1) => a+b;
 
-console.log(addArrow(2,5));
+// console.log(addArrow(2,5));
 
-const printOutput:  (output: string | number) => void = output => console.log('printOutput ' + output);
+// const printOutput:  (output: string | number) => void = output => console.log('printOutput ' + output);
 
-const button = document.querySelector('button');
+// const button = document.querySelector('button');
 
-if(button) {
-    button.addEventListener('click', event =>console.log(event));
-}
+// if(button) {
+//     button.addEventListener('click', event =>console.log(event));
+// }
 
-printOutput(addArrow(5));
+// printOutput(addArrow(5));
 
 
 const hobbies = ['Sports', 'Cooking'];
@@ -29,3 +29,13 @@ const person2 = {
 const copiedPerson = {...person2 };
 
 console.log(copiedPerson);
+//튜플과 함께 사용할 수 있음. 
+ const addArrow = (...numbers: [number, number,number]) => {
+   return numbers.reduce((curResult, curValue) => {
+        return curResult + curValue;
+    }, 0);
+ };
+
+ const addedNumbers = addArrow(5,10,2);
+ console.log(addedNumbers);
+ 
