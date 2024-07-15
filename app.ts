6d0271f8aeae1930a4876b339cba71a20adcc233
sohee <sh663746@kpcard.co.kr>
2024-07-15 09:33:58 +0900

    20.열거형으로 작업하기

--- app.ts
+++ app.ts
@@ -2,23 +2,35 @@
 //     name : string,
 //     age : number
 // } = {
- const person :{
-    name : string,
-    age: number,
-    hobbies : string[],
-    //Tuple : 타입스크립트에 정확히 두 개의 요소가 있는 특별한 배열
-    role : [number,string] 
- } ={
-    name : "sohee",
-    age :30,
-    hobbies : ['Sports', 'Cooking'],
-    role :[2, 'author'],
-};
+//  const person :{
+//     name : string,
+//     age: number,
+//     hobbies : string[],
+//     //Tuple : 타입스크립트에 정확히 두 개의 요소가 있는 특별한 배열
+//     role : [number,string] 
+//  } ={
+//     name : "sohee",
+//     age :30,
+//     hobbies : ['Sports', 'Cooking'],
+//     role :[2, 'author'],
+// };
 //Tuple에서 허용되는 일종의 예외, 타입스크립트가 이 오류를 잡지 못함. 
 //person.role.push('admin');
 //person.role[1] = 10;
 
 //person.role = [0, 'admin', 'user'];
+
+
+enum Role {
+    ADMIN, READ_ONLY, AUTHOR
+};
+
+const person = {
+    name : "sohee",
+    age :30,
+    hobbies : ['Sports', 'Cooking'],
+    role : Role.ADMIN,
+};
 
 let favoriteActivities: string[];
 
@@ -30,4 +42,8 @@ console.log(person.name);
 for(const hobby of person.hobbies) {
     console.log(hobby.toUpperCase());
     
+}
+
+if(person.role == Role.ADMIN) {
+    console.log('is admin')
 }
