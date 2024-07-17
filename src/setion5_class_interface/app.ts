df92dcbad1134572884a24d4d5e6c17522e1caa8
sohee <sh663746@kpcard.co.kr>
2024-07-17 14:58:47 +0900

    62.접근제어자

--- src/setion5_class_interface/app.ts
+++ src/setion5_class_interface/app.ts
@@ -1,5 +1,7 @@
 class Department {
     name: string;
+    //접근제어자 private : 클래스 내에서만 접근 가능.
+    private employees: string [] = [];
     //생성하는 객체의 초기화 작업
     constructor(n: string) {
         this.name = n;
@@ -9,9 +11,31 @@ class Department {
         console.log('Department : ' + this.name);
         
     }
+
+    addEmployee(employee: string) {
+        this.employees.push(employee);
+    }
+
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+        
+        
+    }
 }
 
 const accounting = new   Department('Accounting');
+
+accounting.addEmployee('sohee');
+accounting.addEmployee('dodam');
+
+accounting.name = 'public 접근제어자';
+
+//private 프로퍼티인 employees를 Department 클래스 안에서만 액세스할 수 있음. 
+//accounting.employees[2] = 'Anna';
+
+accounting.describe();
+accounting.printEmployeeInformation();
 
 console.log(accounting);
 
@@ -19,8 +43,8 @@ accounting.describe();
 //객체 리터럴로 생성되는 객체에 describe 프로퍼티를 추가했기 때문. 
 //미리 정의된 클래스를 기반으로 생성되는 객체 ❌ >> 더미객체일뿐 
 //describe 프로퍼티가 accounting 객체 describe 메서드를 참조하는 포인터 
-const accountingCopy = {name : 'sohee', describe: accounting.describe};
-accountingCopy.describe();
+// const accountingCopy = {name : 'sohee', describe: accounting.describe};
+// accountingCopy.describe();
 
 
 
